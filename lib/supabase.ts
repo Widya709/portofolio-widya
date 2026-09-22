ece64ec6f157621378e25594189f68b3938f487d
unknown <widyaaulia709@gmail.com>
2026-09-22 09:25:13 +0700

    fix supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://ygrqvsemkbpgkvyjjwru.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_8ouj_7x655lUrrmr3ajcZQ_K2kKSiVS";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-
-
